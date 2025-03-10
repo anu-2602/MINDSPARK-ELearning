ea9a5ed1974fd028e5cef6007a76b0d2afa9c680
anu-2602 <anushkaunde2602@gmail.com>
2025-03-10 22:57:39 +0530

    contact databse connection done

---
+++ php/contact-form.php
@@ -0,0 +1,31 @@
+<?php
+$servername = "localhost"; // Change if your server is different
+$username = "root"; // Change according to your configuration
+$password = ""; // Change according to your configuration
+$dbname = "mindspark_db"; // Your database name
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Collecting form data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$Interested_Subject = $_POST['interest'];
+$message = $_POST['message'];
+
+// Insert data into database
+$sql = "INSERT INTO contact (name, email, interest, message) VALUES ('$name', '$email' ,'$Interested_Subject' ,'$message')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully. <a href='fetch.php'>View Records</a>";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
